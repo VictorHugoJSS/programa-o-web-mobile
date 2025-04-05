1d8b1a95c9c59826bc51437f7ca28624c88ccd50
VictorHugoJSS <vh9325862@gmail.com>
2025-04-05 20:12:15 -0300

    Password Game feito

---
+++ portifolio/script.js
@@ -0,0 +1,64 @@
+if (window.location.href === window.location.origin + "/portifolio/jogoDaSenha.html"){
+    function GenerateCode(){
+        let pass = [];
+
+        while (pass.length < 6){
+            let num = Math.floor(Math.random() * 10);
+
+            while (!pass.includes(num)){
+                pass.push(num);
+            }
+        }
+        return pass.join('')
+    }
+
+    const codigo = GenerateCode();
+    let tentativas = 0;
+
+    function verificandoAcertosErros(pass, resposta){
+        let numCertos = 0;
+        let numErrados = 0;
+
+        for (let i = 0; i < 6; i++){
+            if (pass[i] === resposta[i]){
+                numCertos++;
+            }
+            else{
+                numErrados++;
+            }
+        }
+
+        return { numCertos, numErrados };
+    }
+
+    function History(resposta, acertos, erros){
+        const tentativaPassada = document.createElement('li');
+        const anterior = document.getElementById('tentativas');
+        tentativaPassada.innerHTML = resposta + ' Acertou: ' + acertos + ' Errou: ' + erros;
+
+        if (anterior.firstChild){
+            anterior.insertBefore(tentativaPassada, anterior.firstChild);
+        }
+        else{
+            anterior.appendChild(tentativaPassada);
+        }
+        tentativas++;
+    }
+
+    function verificandoResposta(){
+        let res = document.getElementById("res").value;
+        let pas = codigo.split('');
+        let arr = res.split('');
+        const { numCertos, numErrados } = verificandoAcertosErros(pas, arr);
+        History(res, numCertos, numErrados);
+        if (acertos === 6 && tentativas <= 10){
+            alert("Você acertou!!!");
+            alert("Deseja jogar novamente? (Pressione F5 para reiniciar)");
+        }
+        else if (tentativas > 10){
+            alert("Que pena. Você perdeu! :( ");
+            alert("A resposta era: " + codigo);
+            alert("Deseja jogar novamente? (Pressione F5 para reiniciar)");
+        }
+    }
+}
